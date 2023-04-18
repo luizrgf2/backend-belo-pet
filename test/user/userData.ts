4ad7119057e66b6cd3e9c3242dfbc20d5af653a7
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-04-18 15:23:43 -0300

    ⚡  REFACTOR: add id to users

--- test/user/userData.ts
+++ test/user/userData.ts
@@ -1,32 +1,37 @@
 import { UserInterface } from "src/app/domain/entities/user";
 
 export const validUser = {
+    id:"1",
     createdAt:new Date(),
-    updateAt: new Date(),
+    updatedAt: new Date(),
     email:"teste@gmail.com",
     password:"Teste123",
     name:"Fernando Felipe"
 } as UserInterface
 
+
 export const invalidUserWithEmailInvalid = {
+    id:"2",
     createdAt:new Date(),
-    updateAt: new Date(),
+    updatedAt: new Date(),
     email:"testegmail.com",
     password:"Teste123",
     name:"Fernando Felipe"
 } as UserInterface
 
 export const invalidUserWithPasswordLenInvalid = {
+    id:"3",
     createdAt:new Date(),
-    updateAt: new Date(),
+    updatedAt: new Date(),
     email:"teste@gmail.com",
     password:"Teste12",
     name:"Fernando Felipe"
 } as UserInterface
 
 export const invalidUserWithPasswordUpperCaseInvalid = {
+    id:"4",
     createdAt:new Date(),
-    updateAt: new Date(),
+    updatedAt: new Date(),
     email:"teste@gmail.com",
     password:"teste123",
     name:"Fernando Felipe"
