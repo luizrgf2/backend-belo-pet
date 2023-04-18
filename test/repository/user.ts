6d928ba8855e542a70c2f4ab3522aa1a75bb3bd5
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-04-18 15:24:13 -0300

    📦  FEAT: userRepoInMemory implemented

---
+++ test/repository/user.ts
@@ -0,0 +1,30 @@
+import { Either, Left, Right } from "../../src/app/app/errors/either";
+import { ErrorBase } from "../../src/app/app/errors/errorBase";
+import { UserNotExists } from "../../src/app/app/errors/user/userNotExists";
+import { createUserUserRepo, userRepositoryImp } from "../../src/app/app/interfaces/repository/user";
+import { UserEntity, UserInterface } from "../../src/app/domain/entities/user";
+import { validUser } from "../user/userData";
+
+export class UserRepoInMemory implements userRepositoryImp{
+    
+    user:UserInterface[] = []
+    
+    async create (user: createUserUserRepo) : Promise<Either<ErrorBase, UserEntity>>{
+        this.user.push(validUser)
+        return Right.create(new UserEntity(this.user[0]))
+    }
+
+    async findById (id: string) : Promise<Either<ErrorBase, UserEntity>>{
+        const userFind = this.user.find(user=>user.id === id)
+        if(!userFind) return Left.create(new UserNotExists())
+        return Right.create(new UserEntity({...userFind}))
+    }
+
+    async findByEmail (email: string) : Promise<Either<ErrorBase, UserEntity>>{
+        const userFind = this.user.find(user=>user.email === email)
+        if(!userFind) return Left.create(new UserNotExists())
+        return Right.create(new UserEntity({...userFind}))
+    }
+
+
+}
