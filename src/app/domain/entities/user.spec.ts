b31c3689c05cf96d19c39e5fcee44fe1a8931cd4
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-04-18 17:24:56 -0300

    ✅  TEST: new tests to check fields is null

--- src/app/domain/entities/user.spec.ts
+++ src/app/domain/entities/user.spec.ts
@@ -1,5 +1,7 @@
-import { invalidUserWithEmailInvalid, invalidUserWithPasswordLenInvalid, invalidUserWithPasswordUpperCaseInvalid, validUser } from "../../../../test/user/userData"
+import { invalidUserWithEmailInvalid, invalidUserWithEmailNull, invalidUserWithNameNull, invalidUserWithPasswordLenInvalid, invalidUserWithPasswordNull, invalidUserWithPasswordUpperCaseInvalid, validUser } from "../../../../test/user/userData"
+import { InvalidNameError } from "../errors/invalidName"
 import { InvalidEmailError } from "../errors/user/invalidEmail"
+import { InvalidPasswordError } from "../errors/user/invalidPassword"
 import { InvalidPasswordLenError } from "../errors/user/invalidPasswordLen"
 import { InvalidPasswordUpperLetterError } from "../errors/user/invalidPasswordUpperLetter"
 import { UserEntity } from "./user"
@@ -29,6 +31,25 @@ describe("tests to userEntity", function(){
         const userData = invalidUserWithPasswordUpperCaseInvalid
         const sut = UserEntity.create(userData)
         expect(sut).toBeInstanceOf(InvalidPasswordUpperLetterError)
+    })
+
+    it("should be able to return an error if the password if null", function(){
+        const userData = invalidUserWithPasswordNull
+        const sut = UserEntity.create(userData)
+        expect(sut).toBeInstanceOf(InvalidPasswordError)
+    })
+
+    it("should be able to return an error if the email if null", function(){
+        const userData = invalidUserWithEmailNull
+        const sut = UserEntity.create(userData)
+        expect(sut).toBeInstanceOf(InvalidEmailError)
+    })
+
+
+    it("should be able to return an error if the name if null", function(){
+        const userData = invalidUserWithNameNull
+        const sut = UserEntity.create(userData)
+        expect(sut).toBeInstanceOf(InvalidNameError)
     })
 
 })
