f280223e564d154ce9d04f2dc43982849133cfb3
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-04-24 20:32:13 -0300

    🛠  REFACTOR: change return from method decode

--- src/app/app/interfaces/encryptor/jwt.ts
+++ src/app/app/interfaces/encryptor/jwt.ts
@@ -1,4 +1,7 @@
+import { Either } from "../../errors/either"
+import { ErrorBase } from "../../errors/errorBase"
+
 export interface JWTImp{
     encode:<T=any>(data:T)=>string
-    decode:<T=any>(token:string)=>T
+    decode:<T=any>(token:string)=>Either<ErrorBase,T>
 }
