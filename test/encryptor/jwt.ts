c9364d2ef3de5d69767ecd2ceb06230c07cfef79
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-04-24 20:30:11 -0300

    🛠  REFACTOR: JWTInMemory decode method return changed

--- test/encryptor/jwt.ts
+++ test/encryptor/jwt.ts
@@ -1,11 +1,21 @@
+import { Either, Right } from "../../src/app/app/errors/either";
+import { ErrorBase } from "src/app/app/errors/errorBase";
 import { JWTImp } from "src/app/app/interfaces/encryptor/jwt";
+import { validUser } from "../../test/user/userData";
+
+export enum JWTTokenValidToTest{
+    validUser="validUser"
+}
 
 export class JWTInMemory implements JWTImp{
     encode<T = any>(data: T) : string{
         return "validtoken"
     }
-    decode<T = any>(token: string) : T{
-        return token as any
+    decode<T = any>(token: string) : Either<ErrorBase,T>{
+        if(token === JWTTokenValidToTest.validUser){
+            return Right.create(validUser as any)
+        }
+        return Right.create({} as any)
     }
 
 }
